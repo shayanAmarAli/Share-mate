1eba03c7b9b7580371490894c0663943ff661314
shayanamaralie <shayanamaralie@gmail.com>
2024-01-21 01:05:45 -0800

    Commit code remove

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,19 +2,19 @@
 import FacebookLoginButton from '@/component/FacebookLogin';
 import FacebookLogin from '@greatsumini/react-facebook-login';
 import axios from 'axios';
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { FacebookShareButton, FacebookIcon } from 'react-share';
 
 
 const App = () => {
-    const [userAccessToken, setUserAccessToken] = useState<any>(''); // State to store user access token
-    const [userId, setUserId] = useState<any>();
+    const router = useRouter();
 
     const handleFacebookLogin = () => {
+
         window.FB.login(function (response) {
             if (response.authResponse) {
                 console.log('Welcome!  Fetching your information.... ', response.authResponse.accessToken);
-                setUserAccessToken(response.authResponse.accessToken)
 
                 fetch(`https://login-facebook-sdk.vercel.app/api/login?token=${response.authResponse.accessToken}`)
                     .then((responseUserCre: any) => {
@@ -22,9 +22,9 @@ const App = () => {
                     })
 
                 FB.api('/me', function (response: any) {
-                    setUserId(response.id)
                     console.log('User Id is ' + response.id + '.');
                     console.log('Good to see you, ' + response.name + '.');
+                    router.push("/")
                 });
             } else {
                 console.log('User cancelled login or did not fully authorize.');
@@ -37,13 +37,6 @@ const App = () => {
             <h1>Welcome to Next.js with Facebook Login</h1>
             <button onClick={handleFacebookLogin}>Login with facebook</button>
             <h1>Facebook Share Example</h1>
-            <FacebookShareButton
-                url={"https://www.facebook.com/syedahsanshah.sherazi.9/posts/pfbid0rf47n5WNh2t5AXVidr4kKV13tjoru5AM3UAC1eyyAG8gVa3Yxub8mqaMaFDdoi16l?comment_id=349529657939772&notif_id=1705853557311130&notif_t=comment_mention&ref=notif"}
-                hashtag={"#hashtag"}
-                className="Demo__some-network__share-button"
-            >
-                <FacebookIcon size={32} round />
-            </FacebookShareButton>
         </div>
     )
 }

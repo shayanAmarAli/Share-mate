b21b84ec2121e91708db72759f6ce847b72b0435
shayanamaralie <shayanamaralie@gmail.com>
2024-01-21 01:29:36 -0800

    asdff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,7 @@ export default function Home() {
     <main>
       <div>
         <h1>WELCOME TO SHARE-MATE</h1><br /><br />
-        <h5>Click to start share POSTS to your Facebook Timeline</h5>
-        <h1>Facebook Share Example</h1>
+        <h5>Click below to start share POSTS to your Facebook Timeline</h5>
         <FacebookShareButton
           url={"https://www.facebook.com/syedahsanshah.sherazi.9/posts/pfbid0rf47n5WNh2t5AXVidr4kKV13tjoru5AM3UAC1eyyAG8gVa3Yxub8mqaMaFDdoi16l?comment_id=349529657939772&notif_id=1705853557311130&notif_t=comment_mention&ref=notif"}
           // quote={"フェイスブックはタイトルが付けれるようです"}
